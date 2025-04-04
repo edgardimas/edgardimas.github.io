bd0a36b01d06d3ac13ac5466a8977fde7fe5a5e1
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2025-04-04 18:40:23 +0700

    feat: media

--- script.js
+++ script.js
@@ -55,3 +55,23 @@ function goBack() {
           </div>
     `;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  sections.forEach((section) => {
+    section.classList.add("hidden");
+    observer.observe(section);
+  });
+});
